a76b234fd816ea5b904a53af22ce161718d82d9d
Halbert-Nascimento <halbert.fac@gmail.com>
2024-06-28 09:03:08 -0300

    Pagina de cadastro do splitter criada

--- CTO_manager/urls.py
+++ CTO_manager/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-       path('',views.index, name='home' ),
+       path('',views.cadastro_splitter, name='cadastro_splitter' ),
        path('pag/',views.pag, name='pag' ),
 ]
--- CTO_manager/views.py
+++ CTO_manager/views.py
@@ -1,22 +1,48 @@
+## importações do django
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.contrib import messages
+
+## importando classes da models
+from .models import Splitter
 
 
-# Create your views here.
 
-def index(request):
+def cadastro_splitter(request):
+  """
+    View para cadastrar um novo Splitter.
+
+    Args:
+        request (HttpRequest): A requisição HTTP recebida.
+
+    Returns:
+        HttpResponse: Uma resposta HTTP com o template renderizado e as mensagens de sucesso ou erro.
+  """
+  
   template = loader.get_template('CTO_manager/splitter_form.html')
+
   context = {
-    'titulo' : 'Splitter form',
-    'titulo_form': 'Splitter',        
+    'titulo' : 'Splitter form', # titulo da pagina
+    'titulo_form': 'Splitter', # titulo para formulario
     }
-  if request.method == 'POST':
-    splitter = request.POST['splitter']
+  
+  if request.method == 'POST': ## verifica se o requerimento e do tipo POST
+    splitter_form = request.POST['splitter'] # Obtém o valor do campo 'splitter' do formulário
+    print(f"Spliter selecionado: 1/{splitter_form}") # degub exibir no console
 
-    print(f"Spliter selecionado: 1/{splitter}")
+    if Splitter.objects.filter(splitter_tipo = splitter_form).exists(): # verifica se o splitter ja existe no banco de dados
+      messages.error(request, f"Splitter 1/{splitter_form} ja esta cadastrado em nosso banco de dados!") # salva uma menagem d error temporaria para exibir que ja esta cadstrado o splitter 
+      
+    
+    else:
+      splitter = Splitter(splitter_tipo = splitter_form) # cria a estância do splitter com o valor recebido do formulario
+      splitter.save() # salva no banco de dados
+      messages.success(request, f"Splitter 1/{splitter_form} salvo") # cria uma mensagem temporaria para ser passada para a nova pagina com informação de sucesso
+      return redirect(cadastro_splitter) # # Redireciona para a mesma página (ou para uma página de sucesso), fazendo com que a pagina do formulario seja limpa
 
-  return HttpResponse(template.render(context, request))
+  return HttpResponse(template.render(context, request)) # se não for post exiba a pagina sem alterações
   
 
 def pag(request):
