f161f3d963dd07085e83af0537e27709653598d9
Halbert-Nascimento <halbert.fac@gmail.com>
2024-07-02 10:20:38 -0300

    delet db

--- getData/views.py
+++ getData/views.py
@@ -1,9 +1,20 @@
+## importações do django
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+## importando classes da models
+from CTO_manager.models import Splitter , CtoPrimaria, CtoSecundaria, Cliente
 
 # Create your views here.
 
 def pesquisa(request):
+    template = loader.get_template('getData/pesquisa.html')
+    context = {
+        'titulo': 'Pesquisa form'
+    }
     
 
 
-    return
+    return HttpResponse(template.render(context, request))
